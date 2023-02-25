8e53d1dadac5e256f4f010edff99b5be40b472bf
Pablo Carneiro Elias <pablo.cael@gmail.com>
2023-02-25 13:19:58 +0900

    Update real_polynomials_factor_irreducible_polynomials_degree_1_or_2.d

--- real_polynomials_factor_irreducible_polynomials_degree_1_or_2.d
+++ real_polynomials_factor_irreducible_polynomials_degree_1_or_2.d
@@ -4,4 +4,4 @@ This is a direct consequence of the Fundamental Theorem of Algebra:
 
 Any non constant complex polynomial can be factored into monomials. If the polynomial is real, then every complex root also have a its conjugate as root, because:
 
-$$a \in \mathbb{C}$$
+$ a \in \mathbb{C} $
