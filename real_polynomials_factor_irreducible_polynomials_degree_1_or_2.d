fb5f04d8c3840d453fbc95a4a1d364e5eaee35ea
Pablo Carneiro Elias <pablo.cael@gmail.com>
2023-02-25 13:19:37 +0900

    Create real_polynomials_factor_irreducible_polynomials_degree_1_or_2.d

---
+++ real_polynomials_factor_irreducible_polynomials_degree_1_or_2.d
@@ -0,0 +1,7 @@
+# Real coefficients polynomials can always factor into irreducible polynomials of degree 1 or 2.
+
+This is a direct consequence of the Fundamental Theorem of Algebra:
+
+Any non constant complex polynomial can be factored into monomials. If the polynomial is real, then every complex root also have a its conjugate as root, because:
+
+$$a \in \mathbb{C}$$
